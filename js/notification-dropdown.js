2e66f7d8977467d6087ea240af422ef5dba229f8
rohung <89629390+rohung@users.noreply.github.com>
2023-04-22 13:41:28 -0700

    init commit

---
+++ js/notification-dropdown.js
@@ -0,0 +1,13 @@
+let bell = document.querySelector('.bell');
+let notification_dropdown = document.querySelector('.notification-dropdown');
+
+
+bell.addEventListener('click', () =>{
+    notification_dropdown.classList.toggle('active');
+});
+
+
+
+
+
+
